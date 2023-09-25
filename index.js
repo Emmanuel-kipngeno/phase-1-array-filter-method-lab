c1f28c99864dd7440af4b04c8229c6db644d71bb
emmanuel-kipngeno <immanuelkipngeno7387@gmail.com>
2023-09-25 20:42:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+// Function to find matching drivers by name
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  // Function to find drivers whose names start with the provided letters
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  }
+  
+  // Function to find drivers whose names match the provided string
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+  }
+  
